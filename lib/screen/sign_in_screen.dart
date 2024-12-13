632f2ca95787ed6f2f3655da1bc1662996635784
Bek <bekhruzbek.makhmudov@gmail.com>
2024-12-13 23:03:39 +0500

    Initial commit

---
+++ lib/screen/sign_in_screen.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SignInScreenState();
+  }
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String? _emailError;
+  String? _passwordError;
+
+  bool _obscureText = true;
+
+  void _validateAndSignIn() {
+    setState(() {
+      _emailError = null;
+      _passwordError = null;
+
+      if (_emailController.text.isEmpty) {
+        _emailError = 'This field is required';
+      } else if (!_emailController.text.contains('@')) {
+        _emailError = 'This email does not exist';
+      }
+
+      if (_passwordController.text.isEmpty) {
+        _passwordError = 'This field is required';
+      } else if (_passwordController.text != '123') {
+        _passwordError = 'Password incorrect';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SafeArea(
+          child: Column(
+            children: [
+              const Spacer(flex: 1),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text('Sign in',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge
+                            ?.copyWith(color: Colors.blue)),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text('Sign up',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge
+                            ?.copyWith(color: Colors.grey)),
+                  ),
+                ],
+              ),
+              const Spacer(flex: 2),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  errorText: _emailError,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+              const Spacer(flex: 1),
+              TextField(
+                controller: _passwordController,
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  errorText: _passwordError,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(
+                        () {
+                          _obscureText = !_obscureText;
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Forgot password?',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(color: Colors.grey),
+                  ),
+                ),
+              ),
+              const Spacer(flex: 1),
+              ElevatedButton(
+                onPressed: _validateAndSignIn,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  'Sign in',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall
+                      ?.copyWith(color: Colors.white),
+                ),
+              ),
+              const Spacer(flex: 2),
+              Row(
+                children: [
+                  const Expanded(child: Divider()),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text('or sign in with',
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.copyWith(color: Colors.grey)),
+                  ),
+                  const Expanded(child: Divider()),
+                ],
+              ),
+              const Spacer(flex: 2),
+              _buildSocialButtonWithIcon(
+                  'Sign in with Google', Icons.email, () {}),
+              const Spacer(flex: 1),
+              _buildSocialButtonWithIcon(
+                  'Sign in with Facebook', Icons.facebook, () {}),
+              const Spacer(flex: 1),
+              _buildSocialButtonWithIcon(
+                  'Sign in with Apple', Icons.apple, () {}),
+              const Spacer(flex: 2),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialButtonWithIcon(
+      String text, IconData icon, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                color: Colors.black,
+              ),
+              const SizedBox(width: 8),
+              Text(
+                text,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
